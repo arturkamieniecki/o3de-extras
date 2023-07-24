59457d5fbd54da7a9062663332f6a824c7485968
Artur Kamieniecki <artur.kamieniecki@robotec.ai>
2023-07-24 10:21:52 +0200

    Wrong test
    Signed-off-by: Artur Kamieniecki <artur.kamieniecki@robotec.ai>

--- Test/test_build.sh
+++ Test/test_build.sh
@@ -10,6 +10,8 @@ cd /data/workspace/WarehouseTest
 if cmake --build build/linux --config profile --target WarehouseTest.GameLauncher Editor ; then
     echo "Build succeeded"
     echo "RESULT: ALL TESTS PASSED" # expected result 
+    if ./o3de/python/python.sh -m pytest --build-directory ./WarehouseTest/build/linux/bin/profile/ ./o3de-extras/Gems/ROS2/Code/PythonTests/SmokeTests_Periodic.py ; then
+        echo "GUI test succesfull"
 else
     echo "RESULT: Build failed"
 fi
